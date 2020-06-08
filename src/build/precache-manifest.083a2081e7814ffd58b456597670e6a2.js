946c8e3c5757f59a70bc14736d8955acdb5917f1
malee1 <46596915+malee1@users.noreply.github.com>
2020-06-08 14:52:03 +0100

    first commit

---
+++ src/build/precache-manifest.083a2081e7814ffd58b456597670e6a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408f56375ad47580e51ad061fa1b439b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa9b773a91e130010088",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "bdd06dac742d693035f8",
+    "url": "/static/js/2.37c99016.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.37c99016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9b773a91e130010088",
+    "url": "/static/js/main.fc42a1dd.chunk.js"
+  },
+  {
+    "revision": "f34df4cc19e0da90cba3",
+    "url": "/static/js/runtime-main.578b6095.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
